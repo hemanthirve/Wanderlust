8d753ed435f7ad9a16d173daed9fef583de83630
Hemant Hirve <hemanthirve1319>
2025-06-22 22:36:37 +0530

    Add

--- controllers/listing.js
+++ controllers/listing.js
@@ -27,25 +27,44 @@ module.exports.showListing = async(req,res) => {
     res.render("listings/show.ejs", {listing});
 }
 
-// Remove in line number 31 (next)
-module.exports.createListing = async (req,res) =>{
+// module.exports.createListing = async (req,res, next) =>{
+//     try {
+//         let url = req.file.path;
+//         let filename = req.file.filename;
+//         const newListing = new Listing(req.body.listing);
+//         newListing.owner = req.user._id;
+//         newListing.image = {url, filename};
+//         await newListing.save();
+//         req.flash("success", "New Listing Created!");
+//         res.redirect("/listings");
+//     } catch (err) {
+//         console.error("Error creating listing:", err);
+//         req.flash('error', 'Failed to create listing. Please try again.');
+//         res.redirect('/listings');
+//     }
+
+// }
+module.exports.createListing = async (req, res, next) => {
     try {
-        let url = req.file.path;
-        let filename = req.file.filename;
         const newListing = new Listing(req.body.listing);
         newListing.owner = req.user._id;
-        newListing.image = {url, filename};
+
+        if (req.file) {
+            newListing.image = {
+                url: req.file.path,
+                filename: req.file.filename
+            };
+        }
+
         await newListing.save();
         req.flash("success", "New Listing Created!");
-        res.redirect("/listings");
+        res.redirect(`/listings/${newListing._id}`);
     } catch (err) {
         console.error("Error creating listing:", err);
-        req.flash('error', 'Failed to create listing. Please try again.');
-        res.redirect('/listings');
+        req.flash("error", `Failed to create listing: ${err.message}`);
+        res.redirect("/listings/new");
     }
-
-}
-
+};
 
 
 module.exports.renderEditForm = async (req,res)=> {
