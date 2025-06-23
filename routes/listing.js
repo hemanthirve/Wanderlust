9707d096c769fdd70cda3507f818c770353891b1
Hemant Hirve <hemanthirve1319>
2025-06-23 10:13:03 +0530

    Add

--- routes/listing.js
+++ routes/listing.js
@@ -13,14 +13,14 @@ const upload = multer({ storage });
 router
 .route("/")
 .get( wrapAsync(listingController.index))
- .post(isLoggedIn,  upload.single('listing[image]'), validateListing, wrapAsync(listingController.createListing));
+ .post(isLoggedIn,  upload.single('image'), validateListing, wrapAsync(listingController.createListing));
 
 // New Route
 router.get("/new", isLoggedIn, listingController.renderNewFrom);
 
 router.route("/:id")
 .get( wrapAsync(listingController.showListing))
-.put( isLoggedIn, isOwner, upload.single('listing[image]'), validateListing, wrapAsync(listingController.undateListing))
+.put( isLoggedIn, isOwner, upload.single('image'), validateListing, wrapAsync(listingController.updateListing))
 .delete( isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
 
