c07b63688802bd9cb28ede0d1c0547c6da58159f
Hemant Hirve <hemanthirve1319>
2025-06-23 21:35:40 +0530

    Update

--- controllers/listing.js
+++ controllers/listing.js
@@ -28,22 +28,24 @@ module.exports.showListing = async(req,res) => {
 }
 
 module.exports.createListing = async (req,res, next) =>{
-    try {
         let url = req.file.path;
         let filename = req.file.filename;
+
         const newListing = new Listing(req.body.listing);
+
         newListing.owner = req.user._id;
         newListing.image = {url, filename};
+
         const savedListing = await newListing.save();
         const populatedListing = await Listing.findById(savedListing._id).populate('owner');
-        await newListing.save();
+        // await newListing.save();
         req.flash("success", "New Listing Created!");
         res.redirect("/listings");
-    } catch (err) {
+    
         console.error("Error creating listing:", err);
         req.flash('error', 'Failed to create listing. Please try again.');
         res.redirect('/listings');
-    }
+    
 
 }
 
@@ -58,13 +60,15 @@ module.exports.renderEditForm = async (req,res)=> {
     }
     let originalImageUrl = listing.image.url;
     originalImageUrl = originalImageUrl.replace("/upload", "/upload/w_250");
-    res.render("listings/edit.ejs", { listing, originalImageUrl});
+    res.render("listings/edit.ejs", { listing, originalImageUrl: listing.image});
+    console.log("listing edited");
 }
 
 module.exports.undateListing = async(req, res)=> {
     let { id } = req.params; 
     let listing = await Listing.findByIdAndUpdate(id, {...req.body.listing});
     if(typeof req.file !=="undefined") { 
+    console.log("Uploaded File:", req.file)
     let url = req.file.path;
     let filename = req.file.filename;
     listing.image = { url, filename };
