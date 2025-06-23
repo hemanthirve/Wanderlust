96d186eea429d8c2668fa606672874beea36fc4a
Hemant Hirve <hemanthirve1319>
2025-06-23 18:48:23 +0530

    Update listing.js

--- controllers/listing.js
+++ controllers/listing.js
@@ -34,6 +34,8 @@ module.exports.createListing = async (req,res, next) =>{
         const newListing = new Listing(req.body.listing);
         newListing.owner = req.user._id;
         newListing.image = {url, filename};
+        const savedListing = await newListing.save();
+        const populatedListing = await Listing.findById(savedListing._id).populate('owner');
         await newListing.save();
         req.flash("success", "New Listing Created!");
         res.redirect("/listings");
