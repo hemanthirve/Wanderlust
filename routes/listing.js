e12c074f293b38e45e48e808675ce6d4062a0b94
Hemant Hirve <hemanthirve1319>
2025-06-28 19:11:10 +0530

    Added

--- routes/listing.js
+++ routes/listing.js
@@ -1,74 +1,35 @@
 const express = require("express");
-const router = express.Router();
+const router =  express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
+const { listingSchema } = require("../Schema.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-const listingController = require("../controllers/listings.js");
-const multer = require("multer");
-const { storage } = require("../cloudconfig.js");
-const upload = multer({ storage });
 
-router.route("/").get(wrapAsync(listingController.index)).post(
-  isLoggedIn,
-  upload.single("listing[image]"), //processing of image by multer
-  validateListing,
-  wrapAsync(listingController.createListing)
-);
-// .post((req, res) => {
-//   res.send(req.file);
-// });
+const listingController = require("../controllers/listing.js");
+const multer = require('multer');
+const { cloudinary, storage } = require("../cloudConfig.js");
+const upload = multer({storage: storage });
 
-//New Route
-router.get("/new", isLoggedIn, listingController.renderNewForm);
 
+//Index Route
 router
-  .route("/:id")
-  .get(wrapAsync(listingController.showListing))
-  .put(
-    isLoggedIn,
-    isOwner,
-    upload.single("listing[image]"),
-    validateListing,
-    wrapAsync(listingController.updateListing)
-  )
-  .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
+.route("/")
+.get( wrapAsync(listingController.index))
+ .post(isLoggedIn,  upload.single('listing[image]'), validateListing, wrapAsync(listingController.createListing));
 
-// Index Route
-// router.get("/", wrapAsync(listingController.index));
+// New Route
+router.get("/new", isLoggedIn, listingController.renderNewFrom);
 
-// Show Route
-// router.get("/:id", wrapAsync(listingController.showListing));
+router.route("/:id")
+.get( wrapAsync(listingController.showListing))
+.put( isLoggedIn, isOwner, upload.single('listing[image]'), validateListing, wrapAsync(listingController.updateListing))
+.delete( isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
-//Create Route
-// router.post(
-//   "/",
-//   isLoggedIn,
-//   validateListing,
-//   wrapAsync(listingController.createListing)
-// );
+
 
 // Edit Route
-router.get(
-  "/:id/edit",
-  isLoggedIn,
-  isOwner,
-  wrapAsync(listingController.renderEditForm)
-);
+router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
 
-// Update Route
-// router.put(
-//   "/:id",
-//   isLoggedIn,
-//   isOwner,
-//   validateListing,
-//   wrapAsync(listingController.updateListing)
-// );
 
-// Delete Route
-// router.delete(
-//   "/:id",
-//   isLoggedIn,
-//   isOwner,
-//   wrapAsync(listingController.destroyListing)
-// );
-module.exports = router;
+
+module.exports = router; 
